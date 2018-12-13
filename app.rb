8b38b74e017ea8f0d036958c8928404217d599ce
Janine Luk <janineluk95@gmail.com>
2018-12-13 19:28:20 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :"pirates/new"
+    end
+    
+    get '/new' do
+      erb:"pirates/new"
+    end
+    
+    post "/pirates" do
+      @pirate = Pirate.create(params[:pirate])
+      params[:pirate][:ships].each {|ship_info| Ship.create(ship_info)}
+      @ships = Ship.all
+      erb :"pirates/show"
+    end
 
   end
 end
